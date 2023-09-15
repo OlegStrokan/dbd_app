61892e991a8285166338ab7a6bb3082252fdd7a7
Oleh Strokan <oleg14ua71@gmail.com>
2023-09-15 11:26:17 +0200

    add volumes

--- webapp/channels/src/components/global_header/left_controls/product_menu/product_branding/product_branding.tsx
+++ webapp/channels/src/components/global_header/left_controls/product_menu/product_branding/product_branding.tsx
@@ -5,9 +5,6 @@ import React from "react";
 import styled from "styled-components";
 
 import Heading from "@mattermost/compass-components/components/heading"; // eslint-disable-line no-restricted-imports
-import glyphMap, {
-  ProductChannelsIcon,
-} from "@mattermost/compass-icons/components";
 
 import { useCurrentProduct } from "utils/products";
 
@@ -23,14 +20,11 @@ const ProductBrandingContainer = styled.div`
 const ProductBranding = (): JSX.Element => {
   const currentProduct = useCurrentProduct();
 
-  const Icon = currentProduct?.switcherIcon
-    ? glyphMap[currentProduct.switcherIcon]
-    : ProductChannelsIcon;
-
   return (
     <ProductBrandingContainer tabIndex={0}>
       <img
         width={18}
+        alt="dpd logo"
         src="https://www.dpd.com/wp-content/uploads/sites/226/2020/05/DPD_symbol_black_rgb.png"
       />
       <Heading element="h1" size={200} margin="none">
