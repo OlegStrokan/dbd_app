acb2d77023506859f37dd07dce74de555a16fb1a
Oleh Strokan <oleg14ua71@gmail.com>
2023-12-18 23:09:14 +0100

    v1.4. Add import data service test

--- fido/src/parcel-delivery/app/services/import-manager/tests/smoke.test.ts
+++ fido/src/parcel-delivery/app/services/import-manager/tests/smoke.test.ts
@@ -1,8 +1,53 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { createDbTestingModule } from '../../../../common/db/create-db-module';
+import { ParcelDeliveryRepository } from '../../../../domain/repositories/parcel-delivery';
+import * as fs from 'fs';
+import {IImportManagerService} from "../interfaces";
 import {IParcelDeliveryRepository} from "../../../repositories/parcel-delivery";
+import {ImportDataService} from "../index";
 
+describe('ImportDataService', () => {
+    let importManagerService: IImportManagerService;
+    let parcelDeliveryRepository: IParcelDeliveryRepository;
+    let module: TestingModule;
 
-describe('ImportManagerService', () => {
+    beforeAll(async () => {
+        module = await createDbTestingModule();
 
-    let parcelDeliveryRepository: IParcelDeliveryRepository
-    let importManagerService
-})
+        importManagerService = module.get<IImportManagerService>(ImportDataService);
+        parcelDeliveryRepository = module.get<IParcelDeliveryRepository>(
+            ParcelDeliveryRepository,
+        );
+    });
+
+    afterAll(async () => {
+        await parcelDeliveryRepository.deleteAll();
+        await module.close();
+    });
+
+    it('should fetch data and save to the database', async () => {
+        const testData = {
+            parcelDelivery: [
+                {
+                    id: 2793209,
+                    parcelNumber: 202380,
+                    name: "Parcel 1"
+                },
+                {
+                    id: 12910,
+                    parcelNumber: 272890,
+                    name: "Parcel 2"
+                }
+            ]
+        }
+
+        jest.spyOn(fs, 'readFileSync').mockReturnValue(JSON.stringify(testData));
+
+        await importManagerService.fetchDataAndSaveToDb();
+
+        const savedParcels = await parcelDeliveryRepository.findAll();
+
+        expect(savedParcels).toHaveLength(testData.parcelDelivery.length);
+    });
+});
+
