bb1610ba57e10aad23bea1e11caa857496c64c9e
Oleh Strokan <oleg14ua71@gmail.com>
2024-05-18 14:10:30 +0200

    v9.3. update worker

--- exchange/src/workers/parcel-event/worker.ts
+++ exchange/src/workers/parcel-event/worker.ts
@@ -1,31 +1,25 @@
 import { AppDataSource } from "../../infrastructure/database.config";
 import { ParcelEvent } from "../../infrastructure/entity/parcel-event/index";
-import * as cron from "node-cron";
+import { createWorker } from "../create";
 
-export class ParcelEventWorker {
-  async start() {
-    try {
-      cron.schedule("* * * * * *", async () => {
-        await this.createParcelEvent();
-      });
-    } catch (e) {
-      console.log("Error starting worker", e);
-    }
-  }
 
-  async createParcelEvent() {
-    const parcelEvent = new ParcelEvent();
-    console.log("Creating parcel event");
-    parcelEvent.id = Math.floor(Math.random() * 1000000).toString();
-    parcelEvent.parcelNumber = Math.floor(Math.random() * 1000000).toString();
-    parcelEvent.createdAt = new Date().toISOString();
-    parcelEvent.updatedAt = new Date().toISOString();
-    parcelEvent.weight = parseFloat((Math.random() * 1000).toFixed(2));
-    try {
-      await AppDataSource.manager.save(parcelEvent);
-    } catch (error) {
-      console.log("Error saving parcel event", error);
-    }
-    console.log("Parcel event created", parcelEvent);
+ const createParcelEvent = async () {
+  const parcelEvent = new ParcelEvent();
+  console.log("Creating parcel event");
+  parcelEvent.id = Math.floor(Math.random() * 1000000).toString();
+  parcelEvent.parcelNumber = Math.floor(Math.random() * 1000000).toString();
+  parcelEvent.createdAt = new Date().toISOString();
+  parcelEvent.updatedAt = new Date().toISOString();
+  parcelEvent.weight = parseFloat((Math.random() * 1000).toFixed(2));
+  try {
+    await AppDataSource.manager.save(parcelEvent);
+  } catch (error) {
+    console.log("Error saving parcel event", error);
   }
+  console.log("Parcel event created", parcelEvent);
 }
+
+export const parcelEventWorker = createWorker({
+  worker: createParcelEvent,
+  schedule: "* * * * * *",
+});
