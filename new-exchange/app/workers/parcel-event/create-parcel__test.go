c748393fe35f806af6dd919839d55a5bfe30dbc1
Oleh Strokan <oleg14ua71@gmail.com>
2024-06-04 17:46:40 +0200

    v12.4. add create parcel event test

---
+++ new-exchange/app/workers/parcel-event/create-parcel__test.go
@@ -0,0 +1,33 @@
+package parcel_event
+
+import (
+	"new-exchange/app/infrastructure/db"
+	. "new-exchange/app/infrastructure/entities/parcel-event"
+	"testing"
+)
+
+func TestCreateParcelEvent(t *testing.T) {
+
+	testDbConfig := db.DbConfig{
+		Host:     "localhost",
+		Port:     5434,
+		User:     "stroka01",
+		Password: "test",
+		DbName:   "exchange_test_db",
+	}
+	db := db.ConnectToDb(testDbConfig)
+	db.AutoMigrate(&ParcelEvent{})
+
+	createParcelEvent(db)
+	var parcelEvent ParcelEvent
+	result := db.Last(&parcelEvent)
+	if result.Error != nil {
+		t.Errorf("Failed to create ParcelEvent: %v", result.Error)
+	}
+
+	var newParcelEvent ParcelEvent
+	result = db.Where("id = ?", parcelEvent.Id).First(&newParcelEvent)
+	if result.Error != nil {
+		t.Errorf("Failed to find the new ParcelEvent in the database: %v", result.Error)
+	}
+}
