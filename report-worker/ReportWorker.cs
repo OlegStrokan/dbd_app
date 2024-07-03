8e2c5101385fcc434a798e9531f1dc8c6d751d23
Oleh Strokan <oleg14ua71@gmail.com>
2024-07-03 16:42:23 +0200

    0.18.3. Report worker: finish worker

--- report-worker/ReportWorker.cs
+++ report-worker/ReportWorker.cs
@@ -1,6 +1,9 @@
 using NATS.Client;
 using NATS.Client.JetStream;
 using NCrontab;
+using System.Data.SqlClient;
+using System.Text;
+
 
 namespace report_worker;
 
@@ -8,19 +11,54 @@ public class ReportWorker : BackgroundService
 {
     private readonly ILogger<ReportWorker> _logger;
     private readonly IConnection _natsConnection;
+    private readonly ParcelDeliveryRepository _parcelDeliveryRepository;
     private IJetStream _jetStream;
+    private IJetStreamManagement _jetStreamManagement;
     private readonly string _reportRequestSubject = "report.requests";
     private readonly string _reportResponseSubject = "report.responses";
+    private readonly string _stream = "reports";
     private CrontabSchedule _schedule;
     private DateTime _nextRun;
 
-    public ReportWorker(ILogger<ReportWorker> logger, IConnection natsConnection)
+    public ReportWorker(ILogger<ReportWorker> logger, IConnection natsConnection, ParcelDeliveryRepository parcelDeliveryRepository
+    )
     {
         _logger = logger;
         _natsConnection = natsConnection;
-        _jetStream = _natsConnection.CreateJetStreamContext();
-        _schedule = CrontabSchedule.Parse("0 7 * * *"); // every day at 7 am
+        _parcelDeliveryRepository = parcelDeliveryRepository;
+        
+        InitializeJetStream();
+        
+        _schedule = CrontabSchedule.Parse("*  * * * *"); // every day at 7 am
         _nextRun = _schedule.GetNextOccurrence(DateTime.Now);
+    }
+
+    private void InitializeJetStream()
+    {
+        _jetStream = _natsConnection.CreateJetStreamContext();
+        _jetStreamManagement = _natsConnection.CreateJetStreamManagementContext();
+
+        try
+        {
+            var streams = _jetStreamManagement.GetStreams();
+            bool streamExist = streams.Any(s => s.Config.Name == _stream);
+            if (!streamExist)
+            {
+                var config = StreamConfiguration.Builder()
+                    .WithName(_stream)
+                    .WithSubjects(_reportRequestSubject, _reportResponseSubject)
+                    .Build();
+
+                _jetStreamManagement.AddStream(config);
+                _logger.LogInformation($"Created stream '{_stream} with subjects '{_reportRequestSubject}' and '{_reportResponseSubject}'");
+            }
+        }
+        catch (NATSBadSubscriptionException ex)
+        {
+            _logger.LogError($"Error creating  stream {_stream}: {ex.Message}");
+            throw;
+        }
+
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -75,6 +113,10 @@ public class ReportWorker : BackgroundService
 
     private byte[] GenerateReport()
     {
-        return new byte[3];
+        decimal totalSum = _parcelDeliveryRepository.GetSum();
+
+        byte[] convertedSum = Encoding.UTF8.GetBytes(totalSum.ToString());
+        
+        return convertedSum;
     }
 }
