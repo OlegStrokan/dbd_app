387974df5735e72b50fa5e253111735ebda963d7
Oleh Strokan <oleg14ua71@gmail.com>
2024-07-07 13:08:31 +0200

    v0.20.7 delete parcel import service

--- new-fido/src/parcel-delivery/services/parcel-import/error.ts
+++
@@ -1,7 +0,0 @@
-import { BaseError } from "../../../shared/libs/errors";
-
-export class ImportManagerSaveError extends BaseError {
-  constructor(message: string, payload: Record<string, unknown>) {
-    super({ message, payload });
-  }
-}
--- new-fido/src/parcel-delivery/services/parcel-import/interfaces.ts
+++
@@ -1,6 +0,0 @@
-import { CreateParcelDeliveryInput } from "@app/parcel-delivery/resolver/request-type";
-
-export interface IParcelImportService {
-  saveDataToDatabase(data: CreateParcelDeliveryInput[]): Promise<void>;
-  consumeNatsMessages(): void;
-}
