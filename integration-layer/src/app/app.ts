59751e7cd5590f656727708804b5dbe9e1ffab94
Oleh Strokan <oleg14ua71@gmail.com>
2024-07-10 20:04:44 +0200

    v0.22.2 update main in IL

--- integration-layer/src/app/app.ts
+++ integration-layer/src/app/app.ts
@@ -14,6 +14,7 @@ import {
   createJobWorkerContainer,
 } from "./interface/container/job-worker";
 import { logger } from "./core/services/logger";
+import { IApiContainer, IAppContainer } from "./interface/container/test";
 
 export class IntegrationLayerApp {
   private server: FastifyInstance<
@@ -31,7 +32,12 @@ export class IntegrationLayerApp {
     try {
       this.container = await createApiContainer();
       this.server = Fastify(this.container);
-      this.jobWorkerContainer = await createJobWorkerContainer();
+      const { exchangeDataSource, iLDataSource } = this
+        .container as IApiContainer;
+      this.jobWorkerContainer = await createJobWorkerContainer(
+        iLDataSource,
+        exchangeDataSource
+      );
       this.jobWorkerContainer.workers.forEach(({ worker, name }) => {
         worker.startCronJob().catch((error) => {
           logger.error({ error }, "Error starting cron job:");
