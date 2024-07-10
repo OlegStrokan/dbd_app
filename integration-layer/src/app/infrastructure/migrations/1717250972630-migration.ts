c843f3b9d2dcb70006461a8b27e548ca7cdbc5a3
Oleh Strokan <oleg14ua71@gmail.com>
2024-07-10 20:05:02 +0200

    v0.22.3. update IL migrations

--- integration-layer/src/app/infrastructure/migrations/1717250972630-migration.ts
+++ integration-layer/src/app/infrastructure/migrations/1717250972630-migration.ts
@@ -1,14 +1,15 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migration1717250972630 implements MigrationInterface {
-    name = 'Migration1717250972630'
+  name = "Migration1717250972630";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "log" ("id" character varying NOT NULL, "lastConsumedAt" character varying NOT NULL, CONSTRAINT "PK_350604cbdf991d5930d9e618fbd" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE IF NOT EXISTS "log" ("id" character varying NOT NULL, "lastConsumedAt" character varying NOT NULL, CONSTRAINT "PK_350604cbdf991d5930d9e618fbd" PRIMARY KEY ("id"))`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "log"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "log"`);
+  }
 }
--- integration-layer/src/app/infrastructure/migrations/1720526697779-migration.ts
+++ integration-layer/src/app/infrastructure/migrations/1720526697779-migration.ts
@@ -1,20 +1,29 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class Migration1720526697779 implements MigrationInterface {
-    name = 'Migration1720526697779'
+  name = "Migration1720526697779";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "log" DROP CONSTRAINT "PK_350604cbdf991d5930d9e618fbd"`);
-        await queryRunner.query(`ALTER TABLE "log" DROP COLUMN "id"`);
-        await queryRunner.query(`ALTER TABLE "log" ADD "id" SERIAL NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "log" ADD CONSTRAINT "PK_350604cbdf991d5930d9e618fbd" PRIMARY KEY ("id")`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "log" DROP CONSTRAINT "PK_350604cbdf991d5930d9e618fbd"`
+    );
+    await queryRunner.query(`ALTER TABLE "log" DROP COLUMN "id"`);
+    await queryRunner.query(`ALTER TABLE "log" ADD "id" SERIAL NOT NULL`);
+    await queryRunner.query(
+      `ALTER TABLE "log" ADD CONSTRAINT "PK_350604cbdf991d5930d9e618fbd" PRIMARY KEY ("id")`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "log" DROP CONSTRAINT "PK_350604cbdf991d5930d9e618fbd"`);
-        await queryRunner.query(`ALTER TABLE "log" DROP COLUMN "id"`);
-        await queryRunner.query(`ALTER TABLE "log" ADD "id" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "log" ADD CONSTRAINT "PK_350604cbdf991d5930d9e618fbd" PRIMARY KEY ("id")`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "log" DROP CONSTRAINT "PK_350604cbdf991d5930d9e618fbd"`
+    );
+    await queryRunner.query(`ALTER TABLE "log" DROP COLUMN "id"`);
+    await queryRunner.query(
+      `ALTER TABLE "log" ADD "id" character varying NOT NULL`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "log" ADD CONSTRAINT "PK_350604cbdf991d5930d9e618fbd" PRIMARY KEY ("id")`
+    );
+  }
 }
