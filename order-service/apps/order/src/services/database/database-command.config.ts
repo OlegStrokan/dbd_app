6844771bc3196e3bce030e49a9a92148003d9eea
Oleh Strokan <oleg14ua71@gmail.com>
2024-10-18 17:36:41 +0200

    0.3.9. add database command/query configs

---
+++ order-service/apps/order/src/services/database/database-command.config.ts
@@ -0,0 +1,17 @@
+import { registerAs } from '@nestjs/config';
+import { OrderCommand } from '../../order/command/order-command.entity';
+import { OrderItemCommand } from '../../order/command/order-item-command.entity';
+
+export const DbCommandConfig = registerAs('command_db', () => ({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 6433,
+  username: process.env.DB_USER || 'stroka01',
+  password: process.env.DB_PASSWORD || 'admin',
+  database: process.env.DB_NAME || 'order_command_db',
+  entities: [OrderCommand, OrderItemCommand],
+  synchronize: true,
+  logging: process.env.NODE_ENV === 'development',
+  migrations: [`${__dirname}/../../migrations/command/*{.ts,.js}`],
+  migrationsTableName: 'migrations',
+}));
