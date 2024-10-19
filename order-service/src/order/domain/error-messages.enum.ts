1f0052ff11f90551456f64e8c8869fc480965f44
Oleh Strokan <oleg14ua71@gmail.com>
2024-10-19 18:05:38 +0200

    0.5.3. setup order module, add error messages

---
+++ order-service/src/order/domain/error-messages.enum.ts
@@ -0,0 +1,11 @@
+export enum ErrorMessages {
+    // order-item
+    INVALID_ORDER_ITEM_QUANTITY = '',
+    INVALID_ORDER_ITEM_PRICE = '',
+    // order
+    ORDER_ALREADY_CREATED = '',
+    ORDER_NOT_IN_CREATED_STATE = '',
+    ORDER_ALREADY_COMPLETED = '',
+    ORDER_NOT_COMPLETED = '',
+    INVALID_ORDER_ITEM = '',
+}
