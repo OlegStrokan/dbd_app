54f554a8d308802f41cd14590821751bd9dbc436
Oleh Strokan <oleg14ua71@gmail.com>
2024-10-20 14:47:04 +0200

    0.5.5. complete order domain entity

--- order-service/src/order/domain/order/order.ts
+++ order-service/src/order/domain/order/order.ts
@@ -1,41 +1,117 @@
 import { AggregateRoot } from '@nestjs/cqrs';
-import { IOrderItem } from '../order-item/order-item.interface';
-import { UnprocessableEntityException } from '@nestjs/common';
+import { InternalServerErrorException, UnprocessableEntityException } from '@nestjs/common';
 import { ErrorMessages } from '../error-messages.enum';
 import { OrderItem } from '../order-item/order-item';
 import { OrderCreatedEvent } from '../event/order-created.event';
+import { OrderCompletedEvent } from '../event/order-completed.event';
+import { OrderCancelledEvent } from '../event/order-canceled.event';
+import { OrderShippedEvent } from '../event/order-shipped.event';
+import { OrderItemAddedEvent } from '../event/order-item-added.event';
+import { generateUlid } from 'src/order/libs/domain/generate-ulid';
+import { Optional } from 'src/order/libs/domain/types';
 
-export type OrderEssentialProperties = Readonly<Required<{ id: string; customerId: string; totalAmount: number }>>;
-
-export type OrderOptionalProperties = Partial<{
-    orderDate: Date;
-    status: OrderStatus;
-    deliveryAddress: string;
-    paymentMethod: string;
-    items: IOrderItem[];
+export type OrderEssentialProperties = Required<{
+    id: string;
+    customerId: string;
+    totalAmount: number;
     createdAt: Date;
-    updatedAt: Date;
-    version: number;
-    trackingNumber: string;
-    deliveryDate: Date;
-    feedback: string;
 }>;
 
-export type OrderData = OrderEssentialProperties & Required<OrderOptionalProperties>;
+export type OrderOptionalProperties = Partial<{
+    orderDate?: Date;
+    deliveryAddress?: string;
+    paymentMethod?: string;
+    items?: OrderItem[];
+    createdAt?: Date;
+    status: OrderStatus;
+    updatedAt?: Date;
+    version?: number;
+    trackingNumber?: string;
+    deliveryDate?: Date;
+    feedback?: string;
+}>;
+
+export type OrderData = OrderEssentialProperties & OrderOptionalProperties;
 
 export interface IOrder {
     compareId: (id: string) => boolean;
-    create: () => void;
+    create: (order: OrderEssentialProperties) => void;
     complete: () => void;
     cancel: () => void;
     ship: (trackingNumber: string, deliveryDate: Date) => void;
-    addItem: (item: IOrderItem) => void;
+    addItem: (item: OrderItem) => void;
     commit: () => void;
 }
 
 export class Order extends AggregateRoot implements IOrder {
     constructor(private orderData: OrderData) {
         super();
+    }
+
+    static generateOrderId = () => generateUlid();
+
+    create(orderData: Omit<OrderEssentialProperties, 'id'>): void {
+        const order = new Order({
+            ...orderData,
+            status: OrderStatus.Created,
+            createdAt: new Date(),
+            id: generateUlid(),
+        });
+        order.apply(new OrderCreatedEvent(order.id, order.customerId, order.totalAmount));
+    }
+
+    compareId(id: string): boolean {
+        return id === this.id;
+    }
+
+    complete(): void {
+        const clonedOrder = this.clone();
+        if (!clonedOrder.isValidStatusChange(OrderStatus.Created, OrderStatus.Completed)) {
+            throw new UnprocessableEntityException(ErrorMessages.ORDER_NOT_IN_CREATED_STATE);
+        }
+        clonedOrder.orderData.status = OrderStatus.Completed;
+        clonedOrder.orderData.updatedAt = new Date();
+        clonedOrder.apply(new OrderCompletedEvent(clonedOrder.id, clonedOrder.customerId));
+    }
+
+    cancel(): void {
+        const clonedOrder = this.clone();
+        if (!clonedOrder.isValidStatusChange(clonedOrder.status, OrderStatus.Canceled)) {
+            throw new UnprocessableEntityException(ErrorMessages.ORDER_NOT_CANCELABLE);
+        }
+
+        clonedOrder.orderData.status = OrderStatus.Canceled;
+        clonedOrder.orderData.updatedAt = new Date();
+        clonedOrder.apply(new OrderCancelledEvent(clonedOrder.id, clonedOrder.customerId));
+    }
+
+    ship(trackingNumber: string, deliveryDate: Date): void {
+        const clonedOrder = this.clone();
+        if (!clonedOrder.isValidStatusChange(clonedOrder.status, OrderStatus.Shipped)) {
+            throw new UnprocessableEntityException(ErrorMessages.ORDER_NOT_COMPLETED);
+        }
+
+        clonedOrder.orderData.trackingNumber = trackingNumber;
+        clonedOrder.orderData.deliveryDate = deliveryDate;
+        clonedOrder.orderData.updatedAt = new Date();
+        clonedOrder.apply(
+            new OrderShippedEvent(
+                clonedOrder.id,
+                clonedOrder.orderData.trackingNumber,
+                clonedOrder.orderData.deliveryDate
+            )
+        );
+    }
+
+    addItem(item: OrderItem): void {
+        const clonedOrder = this.clone();
+        if (!item) {
+            throw new InternalServerErrorException(ErrorMessages.INVALID_ORDER_ITEM);
+        }
+        clonedOrder.orderData.items.push(item);
+        clonedOrder.orderData.totalAmount += item.price * item.quantity;
+        clonedOrder.orderData.updatedAt = new Date();
+        clonedOrder.apply(new OrderItemAddedEvent(clonedOrder.id, item.productId, item.quantity, item.price));
     }
 
     get id(): string {
@@ -58,7 +134,7 @@ export class Order extends AggregateRoot implements IOrder {
         return this.orderData.orderDate || new Date();
     }
 
-    get items(): IOrderItem[] {
+    get items(): OrderItem[] {
         return this.orderData.items || [];
     }
 
@@ -74,18 +150,12 @@ export class Order extends AggregateRoot implements IOrder {
         return this.orderData.feedback;
     }
 
-    create(): void {
-        const order = this.clone();
-        if (this.status) {
-            throw new UnprocessableEntityException(ErrorMessages.ORDER_ALREADY_CREATED);
-        }
-        order.orderData.status = OrderStatus.Created;
-        order.orderData.createdAt = new Date();
-        order.apply(new OrderCreatedEvent(order.id, order.customerId, order.totalAmount));
-    }
-
     private clone(): Order {
         return new Order({ ...this.orderData });
+    }
+
+    private isValidStatusChange(currentStatus: string, nextStatus: string): boolean {
+        return VALID_STATUS_CHANGES[currentStatus]?.includes(nextStatus) ?? false;
     }
 }
 
