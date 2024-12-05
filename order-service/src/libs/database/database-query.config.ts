29014f26c24a9f6565774ce2f0c6ccd597f27ae3
Oleh Strokan <oleg14ua71@gmail.com>
2024-12-05 21:39:10 +0100

    0.6.7. added: db connections, helper functions

---
+++ order-service/src/libs/database/database-query.config.ts
@@ -0,0 +1,15 @@
+import { registerAs } from '@nestjs/config';
+
+export const DbQueryConfig = registerAs('queryConnection', () => ({
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT, 10) || 5433,
+    username: process.env.DB_USER || 'stroka01',
+    password: process.env.DB_PASSWORD || 'admin',
+    database: process.env.DB_NAME || 'order_query_db',
+    entities: [],
+    synchronize: true,
+    logging: process.env.NODE_ENV === 'development',
+    migrations: [`${__dirname}/../../migrations/query/*{.ts,.js}`],
+    migrationsTableName: 'migrations',
+}));
