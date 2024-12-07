fc78db216984dac1ecb5289677b7b6f238fad4f3
Oleh Strokan <oleg14ua71@gmail.com>
2024-12-07 15:06:47 +0100

    0.8.5. add all order command hanlers

---
+++ order-service/src/order/application/handlers/command/ship-order.handler.ts
@@ -0,0 +1,28 @@
+import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
+import { ShipOrderCommand } from '../../command/order/ship-order.command';
+import { Inject, NotFoundException } from '@nestjs/common';
+import { IOrderCommandRepository } from 'src/order/domain/order/order-command.repository';
+import { EventBus } from '@nestjs/cqrs';
+import { OrderQueryRepository } from 'src/order/infrastructure/repository/order/order-query.repository';
+import { OrderShippedEvent } from 'src/order/domain/event/order-item/order-shipped.event';
+import { OrderCommandRepository } from 'src/order/infrastructure/repository/order/order-command.repository';
+
+@CommandHandler(ShipOrderCommand)
+export class ShipOrderHandler implements ICommandHandler<ShipOrderCommand, void> {
+    constructor(
+        @Inject(OrderCommandRepository) private orderRepository: IOrderCommandRepository,
+        @Inject(OrderQueryRepository) private orderQueryRepository: OrderQueryRepository,
+        private readonly eventBus: EventBus
+    ) {}
+
+    async execute(command: ShipOrderCommand): Promise<void> {
+        const order = await this.orderQueryRepository.findById(command.id);
+        if (!order) {
+            throw new NotFoundException('Order not found');
+        }
+        order.ship(command.trackingNumber, command.deliveryDate);
+        await this.orderRepository.updateOne(order);
+
+        this.eventBus.publish(new OrderShippedEvent(order.id, command.trackingNumber, command.deliveryDate));
+    }
+}
