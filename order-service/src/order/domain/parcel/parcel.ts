46889997fabed837d5d37aa6f21dac161fc103c5
Oleh Strokan <oleg14ua71@gmail.com>
2024-12-09 23:25:18 +0100

    0.1.9. update parcel domain entity

--- order-service/src/order/domain/parcel/parcel.ts
+++ order-service/src/order/domain/parcel/parcel.ts
@@ -1,29 +1,59 @@
 import { generateUlid } from 'src/libs/generate-ulid';
 import { OrderItem } from '../order-item/order-item';
 
+export type ParcelData = {
+    id: string;
+    trackingNumber: string;
+    weight: number;
+    dimensions: string;
+    orderId: string;
+    items: OrderItem[];
+    createdAt: Date;
+    updatedAt: Date;
+};
 export class Parcel {
-    constructor(
-        public id: string,
-        public trackingNumber: string,
-        public weight: number,
-        public dimensions: string,
-        public orderId: string,
-        public items: OrderItem[] = []
-    ) {}
+    constructor(public readonly parcel: ParcelData) {}
 
-    static addItem(parcel: Parcel, item: OrderItem): Parcel {
+    get id(): string {
+        return this.parcel.id;
+    }
+
+    get trackingNumber(): string {
+        return this.parcel.trackingNumber;
+    }
+
+    get weight(): number {
+        return this.parcel.weight;
+    }
+
+    get dimensions(): string {
+        return this.parcel.dimensions;
+    }
+
+    get orderId(): string {
+        return this.parcel.orderId;
+    }
+
+    get items(): OrderItem[] {
+        return this.parcel.items;
+    }
+
+    static createWithIdAndDate(parcelData: ParcelData): Parcel {
+        return new Parcel({
+            ...parcelData,
+        });
+    }
+    static addItem(parcelItem: Parcel, item: OrderItem): Parcel {
         if (!item) {
             throw new Error('Invalid Order Item');
         }
 
-        return new Parcel(
-            generateUlid(),
-            parcel.trackingNumber,
-            parcel.weight + item.weight,
-            parcel.dimensions,
-            parcel.orderId,
-            [...parcel.items, item]
-        );
+        return new Parcel({
+            id: generateUlid(),
+            ...parcelItem.parcel,
+            weight: parcelItem.parcel.weight + item.weight,
+            items: [...parcelItem.parcel.items, item],
+        });
     }
 
     static createParcels(orderId: string, items: OrderItem[]): Parcel[] {
@@ -38,23 +68,29 @@ export class Parcel {
                     parcels.push(currentParcel);
                 }
 
-                currentParcel = new Parcel(
-                    generateUlid(),
-                    this.generateTrackingNumber(),
-                    item.weight,
-                    LARGE_ITEM_DIMENSIONS,
-                    orderId
-                );
+                currentParcel = new Parcel({
+                    id: generateUlid(),
+                    trackingNumber: this.generateTrackingNumber(),
+                    weight: item.weight,
+                    dimensions: LARGE_ITEM_DIMENSIONS,
+                    orderId,
+                    items,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                });
                 currentParcel = this.addItem(currentParcel, item);
             } else {
-                if (!currentParcel || currentParcel.weight + item.weight > MAX_PARCEL_WEIGHT) {
-                    currentParcel = new Parcel(
-                        generateUlid(),
-                        this.generateTrackingNumber(),
-                        0,
-                        SMALL_ITEM_DIMENSIONS,
-                        orderId
-                    );
+                if (!currentParcel || currentParcel.parcel.weight + item.weight > MAX_PARCEL_WEIGHT) {
+                    currentParcel = new Parcel({
+                        id: generateUlid(),
+                        trackingNumber: this.generateTrackingNumber(),
+                        dimensions: SMALL_ITEM_DIMENSIONS,
+                        orderId,
+                        items,
+                        createdAt: new Date(),
+                        updatedAt: new Date(),
+                        weight: item.weight,
+                    });
                     parcels.push(currentParcel);
                 }
                 currentParcel = this.addItem(currentParcel, item);
