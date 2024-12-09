71887da36426b1cc0babd8608999d28a71c2e88e
Oleh Strokan <oleg14ua71@gmail.com>
2024-12-09 23:22:48 +0100

    0.1.3. add migrations to query db

--- order-service/src/libs/database/query/database-query.datasource.ts
+++ order-service/src/libs/database/query/database-query.datasource.ts
@@ -1,18 +1,21 @@
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
+import { OrderItemQuery } from 'src/order/infrastructure/entity/order-item/query/order-item-query.entity';
+import { ParcelQuery } from 'src/order/infrastructure/entity/parcel/query/parcel-query.entity';
+import { OrderQuery } from 'src/order/infrastructure/entity/order/order-query.entity';
+import { OrderProjection } from 'src/order/infrastructure/entity/order/order-projection.entity';
 
 config();
 
 export default new DataSource({
     type: 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT, 10) || 6433,
+    port: parseInt(process.env.DB_PORT, 10) || 5433,
     username: process.env.DB_USER || 'stroka01',
     password: process.env.DB_PASSWORD || 'admin',
     database: process.env.DB_NAME || 'order_query_db',
-    entities: [],
-    synchronize: true,
+    entities: [OrderQuery, OrderItemQuery, ParcelQuery, OrderProjection],
     logging: process.env.NODE_ENV === 'development',
-    migrations: [`${__dirname}/../../migrations/query/*{.ts,.js}`],
+    migrations: [`${__dirname}/migrations/*{.ts,.js}`],
     migrationsTableName: 'migrations',
 });
