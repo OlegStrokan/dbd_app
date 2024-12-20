96e562238b84f6c7a2948c287af1eb013517ea8f
Oleh Strokan <oleg14ua71@gmail.com>
2024-12-20 22:41:05 +0100

    0.6.6. udpate calculate shipping cost handler

--- order-service/src/order/application/handlers/command/calculate-shipping-cost.handler.ts
+++ order-service/src/order/application/handlers/command/calculate-shipping-cost.handler.ts
@@ -1,29 +1,19 @@
 import { CommandHandler, EventBus, ICommandHandler } from '@nestjs/cqrs';
+import { CreateShippingCostCommand } from 'src/order/domain/order/event/shipping-cost-calculated.event';
 import { ShippingCost } from 'src/order/domain/shipping-cost/shipping-cost';
-import { ShippingCostCommandRepository } from 'src/order/infrastructure/repository/shipping-cost/shipping-cost-command.repository';
-
-export class CreateShippingCostCommand {
-    constructor(
-        public readonly orderId: string,
-        public readonly weight: number,
-        public readonly dimensions: { length: number; width: number; height: number },
-        public readonly shippingOptions: { expressDelivery: boolean; fragileHandling: boolean; insurance: boolean }
-    ) {}
-}
+import { IShippingCostCommandRepository } from 'src/order/domain/shipping-cost/shipping-cost-command.repository';
 
 @CommandHandler(CreateShippingCostCommand)
 export class CreateShippingCostHandler implements ICommandHandler<CreateShippingCostCommand> {
     constructor(
-        private readonly shippingCostCommandRepository: ShippingCostCommandRepository,
+        private readonly shippingCostCommandRepository: IShippingCostCommandRepository,
         private readonly eventBus: EventBus
     ) {}
 
     async execute(command: CreateShippingCostCommand): Promise<ShippingCost> {
-        const shippingCostDomain = ShippingCost.create({
-            ...command,
-        });
-
-        return await this.shippingCostCommandRepository.createShippingCost(shippingCostDomain.data);
+        const { parcels, ...commandWithouParcels } = command;
+        const shippingCost = ShippingCost.create({ ...commandWithouParcels });
+        return await this.shippingCostCommandRepository.create(shippingCost.data);
 
         // this.eventBus.publish(new ShippingCostCreatedEvent(command.orderId, shippingCostDomain.getCalculatedCost()));
     }
