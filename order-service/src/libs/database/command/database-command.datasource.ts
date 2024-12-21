33001eef56edc42b19ef69e1eb4288f9ba665dfe
Oleh Strokan <oleg14ua71@gmail.com>
2024-12-21 21:43:13 +0100

    0.7.1. add new migrations

--- order-service/src/libs/database/command/database-command.datasource.ts
+++ order-service/src/libs/database/command/database-command.datasource.ts
@@ -4,7 +4,7 @@ import { OrderItemCommand } from 'src/order/infrastructure/entity/order-item/com
 import { OrderCommand } from 'src/order/infrastructure/entity/order/order-command.entity';
 import { RepaymentPreferencesCommand } from 'src/order/infrastructure/entity/repayment-preferences/repayment-preferences-command.entity';
 import { ParcelCommand } from 'src/order/infrastructure/entity/parcel/parcel-command.entity';
-import { ShippingCost } from 'src/order/domain/shipping-cost/shipping-cost';
+import { ShippingCostCommand } from 'src/order/infrastructure/entity/shipping-cost/shipping-cost-command.entity';
 
 config();
 
@@ -15,7 +15,7 @@ export default new DataSource({
     username: process.env.DB_USER || 'stroka01',
     password: process.env.DB_PASSWORD || 'admin',
     database: process.env.DB_NAME || 'order_command_db',
-    entities: [OrderCommand, OrderItemCommand, ParcelCommand, RepaymentPreferencesCommand, ShippingCost],
+    entities: [OrderCommand, OrderItemCommand, ParcelCommand, RepaymentPreferencesCommand, ShippingCostCommand],
     logging: process.env.NODE_ENV === 'development',
     migrations: [`${__dirname}/migrations/*{.ts,.js}`],
     migrationsTableName: 'migrations',
